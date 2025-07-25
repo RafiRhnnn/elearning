f151cdd34b6c9b1e92e4c1b27881849054a983dc
MikhsanFutra <mikhsanfutra@gmail.com>
2025-07-26 02:26:52 +0700

    logo di tab

--- resources/views/admin/kelola_pelajaran.blade.php
+++ resources/views/admin/kelola_pelajaran.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kelola Pelajaran</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}">
+    <!-- Jika ingin PNG: <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}"> -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
