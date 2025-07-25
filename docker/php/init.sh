ba19a0875c6e6956bd84a7a643f8f9d9c58ef246
muhamad alfin pratama <muhamadalfinpratamaa@gmail.com>
2025-07-25 11:33:54 +0700

    fix: update Docker setup to include certbot for SSL certificate management and optimize PHP-FPM configuration

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -1,51 +1,24 @@
 #!/bin/bash
-set -x # Tambah ini agar semua perintah di-log saat dijalankan
+set -x
 
-# atur kepemilikan dan hak akses direktori
-# Pastikan www-data memiliki akses yang benar
-chown -R www-data:www-data /var/www/html
 echo "Mengatur hak akses direktori storage dan bootstrap/cache..."
-# Buat folder dan file log jika belum ada
 mkdir -p /var/www/html/storage/logs
 touch /var/www/html/storage/logs/laravel.log
-# Baru set permission
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
 cd /var/www/html
+
 echo "Membaca environment dari file .env..."
-
-# Ambil APP_ENV dari .env file
-APP_ENV=$(grep ^APP_ENV= .env | cut -d '=' -f2 | tr -d '\r')
-
-if [[ -z "$APP_ENV" ]]; then
-    echo "APP_ENV tidak ditemukan di file .env. Menggunakan 'local' sebagai default."
-    APP_ENV=local
-fi
-
+APP_ENV=$(grep '^APP_ENV=' .env | cut -d '=' -f2 | tr -d '\r')
 echo "Environment Laravel: $APP_ENV"
 
 echo "Menjalankan optimisasi Laravel..."
 composer validate --strict
 composer install --optimize-autoloader --no-dev
-
-# Migrate database jika ada yang perlu
 php artisan migrate
 
-# Jalankan perintah artisan sesuai environment
 if [ "$APP_ENV" = "production" ]; then
-    echo "install npm dependencies"
-    npm install
-    sleep 2
-    npm run build
-    sleep 2
-
-    echo "Mode production: menjalankan caching..."
-    php artisan config:clear
-    php artisan cache:clear
-    php artisan view:clear
-    php artisan route:clear
-
     php artisan config:cache
     php artisan route:cache
     php artisan view:cache
@@ -57,5 +30,4 @@ else
     php artisan route:clear
 fi
 
-# Jalankan PHP-FPM agar container tetap aktif
 exec php-fpm8.2 -F
